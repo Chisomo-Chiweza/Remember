687986860ac28eef6cf874602b4b7a94d93c2e89
Chisomo Chiweza <chisomochiwezapersonal@gmail.com>
2022-07-11 15:19:36 +0200

    Implement ViewBinding
    - Enabled viewBinding in Gradle
    - Renamed activity_main.xml layout file to be home.xml layout file.
    - Implemented viewBinding in MainActivity.kt file.

--- app/src/main/java/com/example/remember/MainActivity.kt
+++ app/src/main/java/com/example/remember/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.remember
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.remember.databinding.HomeBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var homeBinding: HomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val home = homeBinding.root
+        setContentView(home)
     }
 }
